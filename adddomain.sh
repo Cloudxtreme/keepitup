6814873858e58621ffc0651be0f9a316c16041fa
Jan Sarenik <jasan@apiary.io>
2015-04-28 17:36:26 +0200

    Beautify output for the internal-docs

--- adddomain.sh
+++ adddomain.sh
@@ -2,6 +2,7 @@
 
 DOMAIN=$1
 DMAIL="jasan+dmarc"
+DKEY="default._domainkey"
 
 myrw() {
 cat >> /etc/postfix/virtual <<EOF
@@ -14,22 +15,19 @@ echo "Add to mydestination in /etc/postfix/main.conf"
 read enter
 
 cat >> /etc/opendkim/KeyTable <<EOF
-default._domainkey.$DOMAIN $DOMAIN:default:/etc/opendkim/keys/default.private
+$DKEY.$DOMAIN $DOMAIN:default:/etc/opendkim/keys/default.private
 EOF
 
 cat >> /etc/opendkim/SigningTable <<EOF
-*@$DOMAIN default._domainkey.$DOMAIN
+*@$DOMAIN $DKEY.$DOMAIN
 EOF
 }
 
 myrw
 
 cat <<EOF
-mail.apiary-internal.com
-v=spf1 mx -all
-_dmarc
-v=DMARC1; p=none; rua=mailto:$DMAIL@$DOMAIN
-
-default._domainkey
-p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDJ+UkZV21r/+K3cTK9ahBkTuNF3jF68wouTnbU+83n1iS+1CpzGPBOE3VWuwrSs1HRTNz01xo7DuR740Z8AdWaXCdC6msjxAZemQEDklzmmAZkDlnaER9vTBs8QQrCZZt4ULr8MCTaI6Ov51Wq6Yq+6ZkDwOxEWREit0+E0EZ0LQIDAQAB
+@                  IN MX  10 mail.apiary-internal.com
+@                  IN TXT "v=spf1 mx -all"
+_dmarc             IN TXT "v=DMARC1; p=none; rua=mailto:$DMAIL@$DOMAIN"
+$DKEY     IN TXT "p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDJ+UkZV21r/+K3cTK9ahBkTuNF3jF68wouTnbU+83n1iS+1CpzGPBOE3VWuwrSs1HRTNz01xo7DuR740Z8AdWaXCdC6msjxAZemQEDklzmmAZkDlnaER9vTBs8QQrCZZt4ULr8MCTaI6Ov51Wq6Yq+6ZkDwOxEWREit0+E0EZ0LQIDAQAB"
 EOF
